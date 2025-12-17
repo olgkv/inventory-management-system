4fdcc0e260bc034dd8e55045df40f58169d1ff00
Oleg Suchkov <als.b.software@gmail.com>
2025-12-17 16:16:55 +0300

    chore: add ESLint flat config with TypeScript, Prettier, and React Hooks support

---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const prettierPlugin = require('eslint-plugin-prettier');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
+const reactHooks = require('eslint-plugin-react-hooks');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+/** @type {import('eslint').Linter.Config[]} */
+module.exports = [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'pgdata/**'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['**/eslint.config.js'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...(eslintConfigPrettier.rules ?? {}),
+
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-require-imports': 'off',
+
+      'sort-imports': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  {
+    files: ['frontend/**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...(reactHooks.configs.recommended.rules ?? {}),
+    },
+  },
+  {
+    files: ['**/eslint.config.js'],
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    languageOptions: {
+      parserOptions: {
+        sourceType: 'commonjs',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+];
